6e4be774322cf9a516e4d6c553e0fe6ff8a3f4d5
Anemiq <grododzierzca@gmail.com>
2021-02-09 20:47:45 +0100

    Added NOP tests

--- cpu6502/tests/test_ldy.py
+++ cpu6502/tests/test_ldy.py
@@ -1,14 +1,14 @@
 import pytest
 
 
-class TestLDX:
+class TestLDY:
 
     @pytest.mark.parametrize('value, zero_flag, neg_flag', [(0x1, False, False),
                                                             (0x20, False, False),
                                                             (0xff, False, True),
                                                             (0x0, True, False)])
-    def test_ldx_immediate(self, setup_cpu, value, zero_flag, neg_flag):
-        setup_cpu.memory[0x0200] = 0xa0  # LDX instruction
+    def test_ldy_immediate(self, setup_cpu, value, zero_flag, neg_flag):
+        setup_cpu.memory[0x0200] = 0xa0  # LDY instruction
         setup_cpu.memory[0x0201] = value
         setup_cpu.execute(1)
         assert setup_cpu.ldy == value
@@ -21,8 +21,8 @@ class TestLDX:
                                                             (0xff, False, True),
                                                             (0x0, True, False)])
     @pytest.mark.parametrize('zp_address', [0x0, 0x1, 0xff])
-    def test_ldx_zero_page(self, setup_cpu, value, zero_flag, neg_flag, zp_address):
-        setup_cpu.memory[0x0200] = 0xa4  # LDX instruction
+    def test_ldy_zero_page(self, setup_cpu, value, zero_flag, neg_flag, zp_address):
+        setup_cpu.memory[0x0200] = 0xa4  # LDY instruction
         setup_cpu.memory[0x0201] = zp_address
         setup_cpu.memory[zp_address] = value
         setup_cpu.execute(1)
@@ -37,8 +37,8 @@ class TestLDX:
                                                             (0x0, True, False)])
     @pytest.mark.parametrize('zp_address', [0x0, 0x1, 0xef])
     @pytest.mark.parametrize('idx', [0x0, 0x1, 0x10])
-    def test_ldx_zero_page_y(self, setup_cpu, value, zero_flag, neg_flag, zp_address, idx):
-        setup_cpu.memory[0x0200] = 0xb4  # LDX instruction
+    def test_ldy_zero_page_y(self, setup_cpu, value, zero_flag, neg_flag, zp_address, idx):
+        setup_cpu.memory[0x0200] = 0xb4  # LDY instruction
         setup_cpu.memory[0x0201] = zp_address
         setup_cpu.memory[zp_address + idx] = value
         setup_cpu.idy = idx
@@ -53,8 +53,8 @@ class TestLDX:
                                                             (0xff, False, True),
                                                             (0x0, True, False)])
     @pytest.mark.parametrize('address_fst, address_snd', [(0x00, 0x01), (0x01, 0xfe), (0xfa, 0xa1)])
-    def test_ldx_absolute(self, setup_cpu, value, zero_flag, neg_flag, address_snd, address_fst):
-        setup_cpu.memory[0x0200] = 0xac  # LDX instruction
+    def test_ldy_absolute(self, setup_cpu, value, zero_flag, neg_flag, address_snd, address_fst):
+        setup_cpu.memory[0x0200] = 0xac  # LDY instruction
         setup_cpu.memory[0x0201] = address_snd
         setup_cpu.memory[0x0202] = address_fst
         address = address_snd + (address_fst << 8)  # Little endian -> least significant byte first
@@ -71,8 +71,8 @@ class TestLDX:
                                                             (0x0, True, False)])
     @pytest.mark.parametrize('address_fst, address_snd', [(0x00, 0x01), (0x01, 0xac), (0xfa, 0xa1)])
     @pytest.mark.parametrize('idx', [0x0, 0x1, 0x10])
-    def test_ldx_absolute_y_no_page_crossed(self, setup_cpu, value, neg_flag, zero_flag, address_snd, address_fst, idx):
-        setup_cpu.memory[0x0200] = 0xbc  # LDX instruction
+    def test_ldy_absolute_y_no_page_crossed(self, setup_cpu, value, neg_flag, zero_flag, address_snd, address_fst, idx):
+        setup_cpu.memory[0x0200] = 0xbc  # LDY instruction
         setup_cpu.memory[0x0201] = address_snd
         setup_cpu.memory[0x0202] = address_fst
         address = address_snd + (address_fst << 8)  # Little endian -> least significant byte first
@@ -89,8 +89,8 @@ class TestLDX:
                                                             (0x0, True, False)])
     @pytest.mark.parametrize('address_fst, address_snd', [(0x00, 0xff), (0x01, 0xfe), (0xfa, 0xf1)])
     @pytest.mark.parametrize('idx', [0x0f, 0x10])
-    def test_ldx_absolute_y_page_crossed(self, setup_cpu, value, neg_flag, zero_flag, address_snd, address_fst, idx):
-        setup_cpu.memory[0x0200] = 0xbc  # LDX instruction
+    def test_ldy_absolute_y_page_crossed(self, setup_cpu, value, neg_flag, zero_flag, address_snd, address_fst, idx):
+        setup_cpu.memory[0x0200] = 0xbc  # LDY instruction
         setup_cpu.memory[0x0201] = address_snd
         setup_cpu.memory[0x0202] = address_fst
         address = address_snd + (address_fst << 8)  # Little endian -> least significant byte first
---
+++ cpu6502/tests/test_nop.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+class TestNOP:
+
+    @pytest.mark.parametrize('pc', [0x0000, 0x0001, 0x0fff, 0xffff])
+    def test_nop_implied(self, setup_cpu, pc):
+        setup_cpu.pc = pc
+        setup_cpu[setup_cpu.pc] = 0xea  # NOP instruction
+        setup_cpu.execute(1)
+        assert setup_cpu.pc == pc + 1
+        assert setup_cpu.clock.total_clock_cycles == 2
