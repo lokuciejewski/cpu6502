4d38a27673525f2ec3e2a3c7b6bad33ab3c95416
Anemiq <grododzierzca@gmail.com>
2021-02-10 18:13:57 +0100

    Added transfer tests + instructions

--- cpu6502/instructions.py
+++ cpu6502/instructions.py
@@ -7,17 +7,21 @@ class Instructions:
     def __init__(self, cpu, filepath: str):
         self.opcodes = {}
         self.internal_assignment = {
-            'RES': RES,
             'LDA': LDA,
-            'JSR': JSR,
-            'RTS': RTS,
-            'JMP': JMP,
             'LDX': LDX,
             'LDY': LDY,
-            'NOP': NOP,
             'STA': STA,
             'STX': STX,
-            'STY': STY
+            'STY': STY,
+            'TAX': TAX,
+            'TAY': TAY,
+            'TXA': TXA,
+            'TYA': TYA,
+            'JMP': JMP,
+            'JSR': JSR,
+            'RTS': RTS,
+            'NOP': NOP,
+            'RES': RES,
         }
         self.__parse_instruction_json(filepath)
         self.cpu = cpu
@@ -323,24 +327,68 @@ class TAX(AbstractInstruction):
 
     def __init__(self, cpu):
         super().__init__(cpu)
+        self.opcodes = {
+            '0xaa': self.implied
+        }
+
+    def implied(self):
+        self.cpu.idx = self.cpu.acc
+        ~self.cpu.clock
+
+    def finalise(self):
+        self.cpu.ps['zero_flag'] = (self.cpu.idx == 0)
+        self.cpu.ps['negative_flag'] = self.cpu.idx > 0b01111111  # Set negative flag if bit 7 of acc is set
 
 
 class TAY(AbstractInstruction):
 
     def __init__(self, cpu):
         super().__init__(cpu)
+        self.opcodes = {
+            '0xa8': self.implied
+        }
+
+    def implied(self):
+        self.cpu.idy = self.cpu.acc
+        ~self.cpu.clock
+
+    def finalise(self):
+        self.cpu.ps['zero_flag'] = (self.cpu.idy == 0)
+        self.cpu.ps['negative_flag'] = self.cpu.idy > 0b01111111  # Set negative flag if bit 7 of acc is set
 
 
 class TXA(AbstractInstruction):
 
     def __init__(self, cpu):
         super().__init__(cpu)
+        self.opcodes = {
+            '0x8a': self.implied
+        }
+
+    def implied(self):
+        self.cpu.acc = self.cpu.idx
+        ~self.cpu.clock
+
+    def finalise(self):
+        self.cpu.ps['zero_flag'] = (self.cpu.acc == 0)
+        self.cpu.ps['negative_flag'] = self.cpu.acc > 0b01111111  # Set negative flag if bit 7 of acc is set
 
 
 class TYA(AbstractInstruction):
 
     def __init__(self, cpu):
         super().__init__(cpu)
+        self.opcodes = {
+            '0x98': self.implied
+        }
+
+    def implied(self):
+        self.cpu.acc = self.cpu.idy
+        ~self.cpu.clock
+
+    def finalise(self):
+        self.cpu.ps['zero_flag'] = (self.cpu.acc == 0)
+        self.cpu.ps['negative_flag'] = self.cpu.acc > 0b01111111  # Set negative flag if bit 7 of acc is set
 
 
 """JUMPS AND CALLS"""
---
+++ cpu6502/tests/test_transfer.py
@@ -0,0 +1,69 @@
+import pytest
+
+
+@pytest.mark.usefixtures('setup_cpu')
+class TestTAX:
+
+    @pytest.mark.parametrize('value, zero_flag, neg_flag', [(0x1, False, False),
+                                                            (0x20, False, False),
+                                                            (0xff, False, True),
+                                                            (0x0, True, False)])
+    def test_tax_implied(self, setup_cpu, value, neg_flag, zero_flag):
+        setup_cpu.acc = value
+        setup_cpu.memory[0x0200] = 0xaa  # TAX instruction
+        setup_cpu.execute(1)
+        assert setup_cpu.idx == value
+        assert setup_cpu.clock.total_clock_cycles == 2
+        assert setup_cpu.ps['negative_flag'] == neg_flag
+        assert setup_cpu.ps['zero_flag'] == zero_flag
+
+
+@pytest.mark.usefixtures('setup_cpu')
+class TestTAY:
+
+    @pytest.mark.parametrize('value, zero_flag, neg_flag', [(0x1, False, False),
+                                                            (0x20, False, False),
+                                                            (0xff, False, True),
+                                                            (0x0, True, False)])
+    def test_tax_implied(self, setup_cpu, value, neg_flag, zero_flag):
+        setup_cpu.acc = value
+        setup_cpu.memory[0x0200] = 0xa8  # TAY instruction
+        setup_cpu.execute(1)
+        assert setup_cpu.idy == value
+        assert setup_cpu.clock.total_clock_cycles == 2
+        assert setup_cpu.ps['negative_flag'] == neg_flag
+        assert setup_cpu.ps['zero_flag'] == zero_flag
+
+
+@pytest.mark.usefixtures('setup_cpu')
+class TestTXA:
+
+    @pytest.mark.parametrize('value, zero_flag, neg_flag', [(0x1, False, False),
+                                                            (0x20, False, False),
+                                                            (0xff, False, True),
+                                                            (0x0, True, False)])
+    def test_tax_implied(self, setup_cpu, value, neg_flag, zero_flag):
+        setup_cpu.idx = value
+        setup_cpu.memory[0x0200] = 0x8a  # TXA instruction
+        setup_cpu.execute(1)
+        assert setup_cpu.acc == value
+        assert setup_cpu.clock.total_clock_cycles == 2
+        assert setup_cpu.ps['negative_flag'] == neg_flag
+        assert setup_cpu.ps['zero_flag'] == zero_flag
+
+
+@pytest.mark.usefixtures('setup_cpu')
+class TestTYA:
+
+    @pytest.mark.parametrize('value, zero_flag, neg_flag', [(0x1, False, False),
+                                                            (0x20, False, False),
+                                                            (0xff, False, True),
+                                                            (0x0, True, False)])
+    def test_tax_implied(self, setup_cpu, value, neg_flag, zero_flag):
+        setup_cpu.idy = value
+        setup_cpu.memory[0x0200] = 0x98  # TYA instruction
+        setup_cpu.execute(1)
+        assert setup_cpu.acc == value
+        assert setup_cpu.clock.total_clock_cycles == 2
+        assert setup_cpu.ps['negative_flag'] == neg_flag
+        assert setup_cpu.ps['zero_flag'] == zero_flag
