01aca3482b2d910f83a7a262aef74f3093f8a43b
Anemiq <grododzierzca@gmail.com>
2021-02-11 20:19:13 +0100

    Added test for ADC immediate

--- cpu6502/tests/test_arithmetic.py
+++ cpu6502/tests/test_arithmetic.py
@@ -1,11 +1,40 @@
+import numpy as np
 import pytest
 
 
 @pytest.mark.usefixtures('setup_cpu')
 class TestADC:
 
-    def test_adc_immediate(self, setup_cpu):
-        pass
+    @pytest.mark.parametrize('acc', [0x0, 0x1, 0xef, 0xff])
+    @pytest.mark.parametrize('value', [0x0, 0x2, 0xf1, 0xfe])
+    @pytest.mark.parametrize('carry_flag', [False, True])
+    def test_adc_immediate(self, setup_cpu, acc, value, carry_flag):
+        setup_cpu.memory[0x0200] = 0x69  # ADC instruction
+        setup_cpu.memory[0x0201] = value
+        setup_cpu.acc = acc
+        setup_cpu.ps['carry_flag'] = carry_flag
+        expected_value = np.ubyte(value + acc + int(carry_flag))
+        expected_carry_flag = value + acc + int(carry_flag) > 0xff
+        expected_zero_flag = expected_value == 0
+        expected_negative_flag = (expected_value & 0b10000000) != 0
+        expected_overflow_flag = ((value >> 7) == (acc >> 7)) != (expected_value >> 7)
+        """
+        x       y       r
+        1.... + 1.... = 0.... -> True
+        0.... + 0.... = 1.... -> True
+        0.... + 1.... = 0.... -> False
+        0.... + 1.... = 1.... -> False
+        1.... + 0.... = 1.... -> False
+        1.... + 0.... = 0.... -> False
+
+        (x == y) != r
+        """
+        setup_cpu.execute(1)
+        assert setup_cpu.acc == expected_value
+        assert setup_cpu.ps['carry_flag'] == expected_carry_flag
+        assert setup_cpu.ps['zero_flag'] == expected_zero_flag
+        assert setup_cpu.ps['negative_flag'] == expected_negative_flag
+        assert setup_cpu.ps['overflow_flag'] == expected_overflow_flag
 
     def test_adc_zero_page(self, setup_cpu):
         pass
