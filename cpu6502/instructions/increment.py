3e11cea5ca578678850d1737a7e86827c3178716
Anemiq <grododzierzca@gmail.com>
2021-02-19 19:51:51 +0100

    Implemented increment instructions

--- cpu6502/instructions/increment.py
+++ cpu6502/instructions/increment.py
@@ -14,16 +14,41 @@ class INC(cpu6502.instructions.AbstractInstruction):
         }
 
     def zero_page(self):
-        pass
+        address = super(INC, self).zero_page()
+        value = int(self.cpu.read_byte(address), base=0)
+        final_value = np.ubyte(value + 1)
+        ~self.cpu.clock
+        self.cpu.write_byte(address, final_value)
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
 
     def zero_page_x(self):
-        pass
+        address = super(INC, self).zero_page_x()
+        value = int(self.cpu.read_byte(address), base=0)
+        final_value = np.ubyte(value + 1)
+        ~self.cpu.clock
+        self.cpu.write_byte(address, final_value)
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
 
     def absolute(self):
-        pass
+        address = super(INC, self).absolute()
+        value = int(self.cpu.read_byte(address), base=0)
+        final_value = np.ubyte(value + 1)
+        ~self.cpu.clock
+        self.cpu.write_byte(address, final_value)
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
 
     def absolute_x(self):
-        pass
+        address = super(INC, self).absolute_x()
+        value = int(self.cpu.read_byte(address), base=0)
+        final_value = np.ubyte(value + 1)
+        ~self.cpu.clock
+        self.cpu.write_byte(address, final_value)
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
+        ~self.cpu.clock
 
 
 class INX(cpu6502.instructions.AbstractInstruction):
@@ -34,7 +59,11 @@ class INX(cpu6502.instructions.AbstractInstruction):
         }
 
     def implied(self):
-        pass
+        final_value = np.ubyte(self.cpu.idx + 1)
+        ~self.cpu.clock
+        self.cpu.idx = final_value
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
 
 
 class INY(cpu6502.instructions.AbstractInstruction):
@@ -45,4 +74,8 @@ class INY(cpu6502.instructions.AbstractInstruction):
         }
 
     def implied(self):
-        pass
+        final_value = np.ubyte(self.cpu.idy + 1)
+        ~self.cpu.clock
+        self.cpu.idy = final_value
+        self.cpu.ps['zero_flag'] = final_value == 0
+        self.cpu.ps['negative_flag'] = (final_value >> 7) == 1
--- cpu6502/tests/test_increment.py
+++ cpu6502/tests/test_increment.py
@@ -26,7 +26,7 @@ class TestINC:
         expected_zero_flag = end_val == 0
         expected_negative_flag = (end_val >> 7) == 1
         setup_cpu.execute(1)
-        assert setup_cpu.memory[0xa4] == end_val
+        assert setup_cpu.memory[0xa4 + 0x10] == end_val
         assert setup_cpu.ps['zero_flag'] == expected_zero_flag
         assert setup_cpu.ps['negative_flag'] == expected_negative_flag
         assert setup_cpu.clock.total_clock_cycles == 6
@@ -40,14 +40,14 @@ class TestINC:
         expected_zero_flag = end_val == 0
         expected_negative_flag = (end_val >> 7) == 1
         setup_cpu.execute(1)
-        assert setup_cpu.memory[0xa4] == end_val
+        assert setup_cpu.memory[0x4645] == end_val
         assert setup_cpu.ps['zero_flag'] == expected_zero_flag
         assert setup_cpu.ps['negative_flag'] == expected_negative_flag
         assert setup_cpu.clock.total_clock_cycles == 6
 
     @pytest.mark.parametrize('start_val, end_val', [(0x0, 0x1), (0x1, 0x2), (0xab, 0xac), (0xfe, 0xff), (0xff, 0x0)])
     def test_inc_absolute_x(self, setup_cpu, start_val, end_val):
-        setup_cpu.memory[0x0200] = 0xee  # INC instruction
+        setup_cpu.memory[0x0200] = 0xfe  # INC instruction
         setup_cpu.memory[0x0201] = 0x45
         setup_cpu.memory[0x0202] = 0x46
         setup_cpu.memory[0x4645 + 0x1e] = start_val
@@ -55,7 +55,7 @@ class TestINC:
         expected_zero_flag = end_val == 0
         expected_negative_flag = (end_val >> 7) == 1
         setup_cpu.execute(1)
-        assert setup_cpu.memory[0xa4] == end_val
+        assert setup_cpu.memory[0x4645 + 0x1e] == end_val
         assert setup_cpu.ps['zero_flag'] == expected_zero_flag
         assert setup_cpu.ps['negative_flag'] == expected_negative_flag
         assert setup_cpu.clock.total_clock_cycles == 7
@@ -82,7 +82,7 @@ class TestINY:
 
     @pytest.mark.parametrize('start_val, end_val', [(0x0, 0x1), (0x1, 0x2), (0xab, 0xac), (0xfe, 0xff), (0xff, 0x0)])
     def test_iny_implied(self, setup_cpu, start_val, end_val):
-        setup_cpu.memory[0x0200] = 0xe8  # INY instruction
+        setup_cpu.memory[0x0200] = 0xc8  # INY instruction
         setup_cpu.idy = start_val
         expected_zero_flag = end_val == 0
         expected_negative_flag = (end_val >> 7) == 1
